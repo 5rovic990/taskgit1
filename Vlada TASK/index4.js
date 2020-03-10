767454d98efbfbd67e335d613ccf5ad830bac0a8
petrovic <petrovic990v@gmail.com>
2020-03-10 16:20:19 +0100

    prva nedelja kursa

---
+++ Vlada TASK/index4.js
@@ -0,0 +1,27 @@
+console.log("Hello world")
+console.log(3)
+console.log(3 + 5)
+console.log(3 - 5)
+
+//promenljiva
+// var, let, const
+
+// let x = 'Hello world'
+// console.log(x);
+
+// x='JavaScript'
+// console.log(x);
+
+let a = 5
+let b = 13 
+let c = 17
+
+console.log(a +=1)
+console.log(a -=1)
+console.log(a*=1)
+console.log(a/=1)
+console.log(2,3,5)
+let d = a * b
+// console.log(d);
+
+
